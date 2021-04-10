d18c88ff2a28f0d6c24adb65dfcf9b8694562f3d
jeyzandra <jeyzandrasilva@gmail.com>
2021-04-10 14:23:42 -0300

    Criando o R do CRUD

---
+++ cadpessoas_jeyzandra (1)/cadpessoas_jeyzandra/src/main/java/br/com/jeyzandra/cadpessoas/CadpessoasJeyzandraApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeyzandra.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJeyzandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJeyzandraApplication.class, args);
+	}
+
+}
