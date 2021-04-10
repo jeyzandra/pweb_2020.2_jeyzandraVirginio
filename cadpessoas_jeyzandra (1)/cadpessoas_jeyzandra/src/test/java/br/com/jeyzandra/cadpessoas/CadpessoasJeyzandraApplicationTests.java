9066898c1e8eb568ffaa7ac29587685d9da0dba5
jeyzandra <jeyzandrasilva@gmail.com>
2021-04-10 14:26:44 -0300

    Criando o R do CRUD

---
+++ cadpessoas_jeyzandra (1)/cadpessoas_jeyzandra/src/test/java/br/com/jeyzandra/cadpessoas/CadpessoasJeyzandraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeyzandra.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasJeyzandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
