1dd5e2a18173761a8e89204f78db75a92f543c51
jeyzandra <jeyzandrasilva@gmail.com>
2021-04-12 15:00:38 -0300

    Finalizando crud

--- cadpessoas_jeyzandra (1)/cadpessoas_jeyzandra/src/main/java/br/com/jeyzandra/cadpessoas/controller/PessoaController.java
+++ cadpessoas_jeyzandra (1)/cadpessoas_jeyzandra/src/main/java/br/com/jeyzandra/cadpessoas/controller/PessoaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,10 +28,48 @@ public class PessoaController {
 	@GetMapping("/listarPessoas")
 	public ModelAndView listarPessoas() {
 		List<Pessoa> lista = pessoaRepo.findAll();
-		ModelAndView mav = new ModelAndView("listarPessoas");
-		mav.addObject("pessoas", lista);
-		return mav;
+		ModelAndView modelAndView = new ModelAndView("listarPessoas");
+		modelAndView.addObject("pessoas", lista);
+		
+		return modelAndView;	
 	}
 	
+	
+	@GetMapping("/adicionarPessoa")
+	public ModelAndView formAdicionarPessoas() {
+		ModelAndView modelAndView = new ModelAndView("adicionarPessoa");
+		modelAndView.addObject(new Pessoa());
+		return modelAndView;
+	}
+	@PostMapping("/adicionarPessoa")
+	public String adicionarPessoa(Pessoa aPessoa) {
+		this.pessoaRepo.save(aPessoa);
+		return "redirect:/listarPessoas";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView formEditarPessoa(@PathVariable("id") long id) {
+		Pessoa pessoa = pessoaRepo.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		
+		ModelAndView modelAndView = new ModelAndView("editarPessoa");
+		modelAndView.addObject(pessoa);
+		return modelAndView;
+	}
+	
+	@PostMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable("id") long id, Pessoa pessoa) {
+		this.pessoaRepo.save(pessoa);
+		return new ModelAndView("redirect:/listarPessoas");
+	}	
+	
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerPessoa(@PathVariable("id") long id) {
+		Pessoa aRemover = pessoaRepo.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		
+		pessoaRepo.delete(aRemover);
+		return new ModelAndView("redirect:/listarPessoas");
+	}
 
 }
